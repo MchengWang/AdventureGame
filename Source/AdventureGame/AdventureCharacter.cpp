3d6fd23bb52f51542623fc46f8479c6aeb46de52
MchengWang <2734190606@qq.com>
2025-06-25 15:50:49 +0800

    Configure role movement

--- Source/AdventureGame/AdventureCharacter.cpp
+++ Source/AdventureGame/AdventureCharacter.cpp
@@ -15,6 +15,22 @@ AAdventureCharacter::AAdventureCharacter()
 void AAdventureCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	check(GEngine != nullptr);
+
+	// 获取此角色的玩家控制器
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		// 获取增强的输入本地玩家子系统，并向其添加新的输入映射上下文
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(FirstPersonContext, 0);
+		}
+	}
+
+	// Display a debug message for five seconds. 
+	// The -1 "Key" value argument prevents the message from being updated or refreshed.
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("We are using AdventureCharacter."));
 	
 }
 
@@ -28,7 +44,33 @@ void AAdventureCharacter::Tick(float DeltaTime)
 // Called to bind functionality to input
 void AAdventureCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	// 检查传递给此函数的 UInputComponent 并将其转换为 UEnhancedInputComponent
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		// 绑定移动行为
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAdventureCharacter::Move);
 
+		// 绑定跳跃行为
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+	}
+}
+
+void AAdventureCharacter::Move(const FInputActionValue& Value)
+{
+	// 从输入作返回的移动值的 2D 向量
+	const FVector2D MovementValue = Value.Get<FVector2D>();
+
+	// 检查拥有此 Actor 的控制器是否有效
+	if (Controller)
+	{
+		// 添加左右行为
+		const FVector Right = GetActorRightVector();
+		AddMovementInput(Right, MovementValue.X);
+
+		// 添加前后行为
+		const FVector Forward = GetActorForwardVector();
+		AddMovementInput(Forward, MovementValue.Y);
+	}
 }
 
--- Source/AdventureGame/AdventureCharacter.h
+++ Source/AdventureGame/AdventureCharacter.h
@@ -4,7 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "EnhancedInputComponent.h" // 添加增强输入组件模块
+#include "EnhancedInputSubsystems.h" // 启动对本地玩家子系统的访问
+#include "InputActionValue.h" // 启用对输入操作所产生的输入操作值的访问
 #include "AdventureCharacter.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+class UInputComponent;
 
 UCLASS()
 class ADVENTUREGAME_API AAdventureCharacter : public ACharacter
@@ -19,11 +26,26 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	TObjectPtr<UInputMappingContext> FirstPersonContext;
+
+	// 移动操作
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	TObjectPtr<UInputAction> MoveAction;
+
+	// 跳跃
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	TObjectPtr<UInputAction> JumpAction;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// 处理 2D 移动操作
+	UFUNCTION()
+	void Move(const FInputActionValue& Value);
 
 };
